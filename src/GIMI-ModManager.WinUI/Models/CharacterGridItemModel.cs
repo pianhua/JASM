a45b7210412dd15f6e905375bd4e90b0b4e8f507
pianhua <18837380523@163.com>
2026-02-08 20:31:07 +0800

    pian

--- src/GIMI-ModManager.WinUI/Models/CharacterGridItemModel.cs
+++ src/GIMI-ModManager.WinUI/Models/CharacterGridItemModel.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿﻿﻿﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using GIMI_ModManager.Core.GamesService.Interfaces;
 using GIMI_ModManager.WinUI.Services.Notifications;
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Media;
 
 namespace GIMI_ModManager.WinUI.Models;
 
@@ -24,6 +26,9 @@ public partial class CharacterGridItemModel : ObservableObject, IEquatable<Chara
 
     [ObservableProperty] private bool _hasMods;
     [ObservableProperty] private bool _hasEnabledMods;
+
+    [ObservableProperty] private Brush _statusBrush = new SolidColorBrush(Colors.Gray);
+    [ObservableProperty] private Brush _modCountBrush = new SolidColorBrush(Colors.Gray);
 
     public ObservableCollection<CharacterModItem> Mods { get; } = new();
 
@@ -48,10 +53,26 @@ public partial class CharacterGridItemModel : ObservableObject, IEquatable<Chara
         HasMods = ModCount > 0;
         HasEnabledMods = enabledMods > 0;
 
-        if (HasEnabledMods)
-            ModCountString = enabledMods == ModCount ? enabledMods.ToString() : $"{enabledMods} / {ModCount}";
+        ModCountString = $"{enabledMods} / {ModCount}";
+
+        if (enabledMods == 0)
+        {
+            StatusBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 128, 128, 128)); // Gray
+            ModCountBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 128, 128, 128)); // Gray
+        }
         else
-            ModCountString = ModCount.ToString();
+        {
+            ModCountBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 30, 144, 255)); // DodgerBlue
+
+            if (enabledMods == 1)
+            {
+                StatusBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 0, 255, 0)); // Neon Green
+            }
+            else
+            {
+                StatusBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 50, 50)); // Bright Red
+            }
+        }
     }
 
     public bool Equals(CharacterGridItemModel? other)
